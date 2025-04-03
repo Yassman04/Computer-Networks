07ea6c197b31858ca42386f33357d9e133d4ed91
Yassman04 <107676816+Yassman04@users.noreply.github.com>
2025-04-03 17:54:24 +0100

    Update AzureLabTest.java

--- AzureLabTest/src/AzureLabTest.java
+++ AzureLabTest/src/AzureLabTest.java
@@ -30,14 +30,13 @@ class AzureLabTest {
             System.err.println("Please set your e-mail address!");
             System.exit(1);
         }
-        String ipAddress = "Put the IP address of Azure lab machine here!  It should start with 10";
+        String ipAddress = "Put the IP address of Azure lab machine here!";
         if (false && ipAddress.indexOf('.') == -1) {
             System.err.println("Please set your ip address!");
             System.exit(1);
         }
 
         try {
-            // Create a node and initialise it
             Node node = new Node();
             String nodeName = "N:" + emailAddress;
             node.setNodeName(nodeName);
@@ -45,12 +44,17 @@ class AzureLabTest {
             int port = 20110;
             node.openPort(port);
 
-            // Wait and hope that we get sent the address of some other nodes
-            System.out.println("Waiting for another node to get in contact");
-            node.handleIncomingMessages(12 * 1000);
+            System.out.println("Waiting for another node to get in contact...");
+            node.handleIncomingMessages(5000);
 
-            // Let's start with a test of reading key/value pairs stored on the network.
-            // This should print out a poem.
+            System.out.println("Checking if other nodes are active...");
+            boolean active = node.isActive("N:" + emailAddress);
+            System.out.println("Other nodes active? " + active);
+
+            System.out.println("Registering node on network...");
+            node.write(nodeName, ipAddress + ":" + port);
+            Thread.sleep(5000);
+
             System.out.println("Getting the poem...");
             for (int i = 0; i < 7; ++i) {
                 String key = "D:jabberwocky" + i;
@@ -63,30 +67,12 @@ class AzureLabTest {
                 }
             }
 
-            // Now let's test writing a key/value pair
-            System.out.println("Writing a marker so it's clear my code works");
-            {
-                String key = "D:" + emailAddress;
-                String value = "It works!";
-                boolean success = node.write(key, value);
-
-                // Read it back to be sure
-                System.out.println(node.read(key));
-            }
-
-            // Finally we will let other nodes know where we are
-            // so that we can be contacted and can store data for others.
-            System.out.println("Letting other nodes know where we are");
-            node.write(nodeName, ipAddress + ":" + port);
-
-            System.out.println("Handling incoming connections");
+            System.out.println("Handling incoming connections...");
             node.handleIncomingMessages(0);
-
 
         } catch (Exception e) {
             System.err.println("Exception during AzureLabTest");
             e.printStackTrace(System.err);
-            return;
         }
     }
 }
